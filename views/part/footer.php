ae03f56222c0025545daf1a27ae80be1b5706680
John Quezada <john_quezada@hotmail.com>
2023-12-01 18:06:03 -0500

    ACTUALIZACION DEL FOOTER
    ACTUALIZACION DEL FOOTER

--- views/part/footer.php
+++ views/part/footer.php
@@ -1,4 +1,21 @@
-        <script src="<?=  BASE_URL . 'asset/plugins/jquery/jquery.min.js'; ?>"></script>
+ <!-- /.content-wrapper -->
+ <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 1
+    </div>
+    <strong>Copyright &copy; 2023 <a href="#">PIZZERIA</a>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+
+<script src="<?=  BASE_URL . 'asset/plugins/jquery/jquery.min.js'; ?>"></script>
         <script src="<?=  BASE_URL . 'asset/plugins/bootstrap/js/bootstrap.bundle.min.js'; ?>"></script>
         <script src="<?=  BASE_URL . 'asset/js/adminlte.min.js'; ?>"></script>
         <?php 
