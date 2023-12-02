a62d14ed26be6b801f1ae01a768d1e994e3f1d71
John Quezada <john_quezada@hotmail.com>
2023-12-01 23:03:17 -0500

    Actualizacion del  usuario
    Actualizacion del  usuario

--- views/users/index.php
+++ views/users/index.php
@@ -6,8 +6,7 @@
   
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
-    <a href="../../index3.html" class="brand-link">
-      <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="#" class="brand-link">
       <span class="brand-text font-weight-light">PIZZA</span>
     </a>
 
