075379f9689b0d8f13135edb9bfe5a159491e1ee
RebecaFrancoC <152455510+RebecaFrancoC@users.noreply.github.com>
2023-12-01 21:29:48 -0500

    Add files via upload

---
+++ views/part/productos.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="loggedin">
+    <style>
+        *{
+            box-sizing: border-box;
+        }
+        section{
+            max-width: 600px;
+            margin: auto;
+            text-align: center;
+            border: 1px solid #000;
+            border-radius: 5px;
+            padding: 10px;
+            background-color: #ffc107;
+        }
+        input{
+            width: 70%;
+            padding: 10px;
+        }
+    </style>
+    <section>
+        <div class="content">
+            <h2>Listado de productos</h2>
+        </div>
+        <div>
+            <table border="1"" align="center">
+                <tr>
+                    <td>ID</td>
+                    <td>NOMBRE</td>
+                    <td>PRECIO</td>
+                    <td>OPCIONES</td>
+                </tr>
+                <?php
+                    $cnx = mysqli_connect(HOST, USERNAME, PASSWORD, DDB_NAME);
+                    $sql = "SELECT id, name, price from products order by id desc";
+                    $rta = mysqli_query($cnx, $sql);
+                    while($mostrar=mysqli_fetch_row($rta)){
+                    ?>
+                    <tr>
+                        <td><?php echo $mostrar['0']?></td>
+                        <td><?php echo $mostrar['1']?></td>
+                        <td><?php echo $mostrar['2']?></td>
+                        <td>
+                            <a href="editar.php?
+                            id=<?php echo $mostrar['0']?> &
+                            name=<?php echo $mostrar['1']?> &
+                            price=<?php echo $mostrar['2']?>
+                            ">Editar</a>
+                        </td>
+                    </tr>
+                    <?php
+                    }
+                    ?>
+            </table>
+        </div>
+    </section>
+</body>
+</html>
---
+++ views/part/sp_editar.php
@@ -0,0 +1,16 @@
+<?php
+$id = $_POST['id'];
+$name = $_POST['name'];
+$price = $_POST['precio'];
+
+$cnx = mysqli_connect(HOST, USERNAME, PASSWORD, DDB_NAME);
+$sql = "UPDATE products set name='$name', price='$price' where id like $id ";
+$rta = mysqli_query($cnx, $sql);
+
+if(!$rta){
+    echo "No se actualizó el producto";
+}
+else{
+    header("Location: productos.php");
+}
+?>
